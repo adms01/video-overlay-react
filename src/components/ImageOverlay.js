ce0305a662036c844dfc6458d49d69d4549db5e4
hali864 <48163863+hali864@users.noreply.github.com>
2021-11-20 20:40:58 +0000

    add limit for image occurance

--- src/components/ImageOverlay.js
+++ src/components/ImageOverlay.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
 
 function ImageOverlay({ startTime, endTime, image, occurance, alt, currentTime, className }) {
-  return <>{currentTime >= startTime && currentTime <= endTime && <img src={image} alt={alt} className={className} />}</>;
+  const [count, setCount] = useState(0);
+
+  const countHandler = () => {
+    setCount(count + 1);
+  };
+
+  const showImage = () => {
+    if (currentTime >= startTime && currentTime <= endTime && count < occurance) return true;
+    return false;
+  };
+
+  return <>{showImage() && <img src={image} alt={alt} className={className} onLoad={countHandler} />}</>;
 }
 
 export default ImageOverlay;
