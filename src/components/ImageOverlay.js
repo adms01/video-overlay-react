0d08fb7812986b3ecd366072fa453742e164e834
hali864 <48163863+hali864@users.noreply.github.com>
2021-11-20 20:38:36 +0000

    display images at given timestamps

--- src/components/ImageOverlay.js
+++ src/components/ImageOverlay.js
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 
 function ImageOverlay({ startTime, endTime, image, occurance, alt, currentTime, className }) {
-  return (
-    <>
-      <img src={image} alt={alt} className={className} />
-    </>
-  );
+  return <>{currentTime >= startTime && currentTime <= endTime && <img src={image} alt={alt} className={className} />}</>;
 }
 
 export default ImageOverlay;
