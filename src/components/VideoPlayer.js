6919310a23c7021bf8861ff36fb7bdb52bffc81d
hali864 <48163863+hali864@users.noreply.github.com>
2021-11-20 20:30:42 +0000

    add currentTimestamps functionality

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,9 +1,17 @@
-import React from "react";
+import { useRef, useState } from "react";
 
 function VideoPlayer() {
+  const videoRef = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const onTimeUpdateHandler = () => {
+    const timestamp = videoRef.current.currentTime * 1000;
+    setCurrentTime(timestamp);
+  };
+
   return (
     <div>
-      <video className="video-player" controls autoPlay>
+      <video className="video-player" ref={videoRef} onTimeUpdate={onTimeUpdateHandler} controls autoPlay>
         <source src="./Big_Buck_Bunny_1080_10s_5MB.mp4" type="video/mp4" /> Sorry, your browser doesn't support embedded videos
       </video>
     </div>
