bcbe6820a99f062f3dc1b4ac13a168e4b20c08de
hali864 <48163863+hali864@users.noreply.github.com>
2021-11-20 20:33:09 +0000

    create images object

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,4 +1,7 @@
 import { useRef, useState } from "react";
+import image1 from "../assets/images/image1.png";
+import image2 from "../assets/images/image2.png";
+import image3 from "../assets/images/image3.png";
 
 function VideoPlayer() {
   const videoRef = useRef(null);
@@ -8,6 +11,12 @@ function VideoPlayer() {
     const timestamp = videoRef.current.currentTime * 1000;
     setCurrentTime(timestamp);
   };
+
+  const images = [
+    { name: "image1", startTime: 3500, endTime: 5000, image: image1, occurance: 1 },
+    { name: "image2", startTime: 6000, endTime: 8000, image: image2, occurance: 2 },
+    { name: "image3", startTime: 7000, endTime: 8500, image: image3, occurance: 3 },
+  ];
 
   return (
     <div>
